acdb1d717ee3cc67c688d4d668c6a6c81681cdb4
Franz <gitkraken@franz.science>
2021-02-14 12:49:44 +0100

    initialise repo

---
+++ README.md
@@ -0,0 +1,13 @@
+# Remove Leifi-Physik results in Google search.
+
+Based on [this w3schools-Remover](https://github.com/GMaiolo/remove-w3schools).  
+
+Has not yet been added to Chrome extensions store.  
+
+Manual Installation: 
+
+1. Clone/download the project
+2. Go to [chrome extensions page](chrome://extensions/)
+3. Enable developer mode
+4. Click on "Load unpacked extension..." button
+5. Select the project folder and load extension.
---
+++ scripts/remover.js
@@ -0,0 +1,85 @@
+(function() {
+    var LeifiRemover = {
+        currentUrl: {}, 
+        constants: {
+            queries: {
+                result_links: '.g > div > div > a[href*="www.leifiphysik.de/"]', 
+                link_parent_node: '#rso div.g', 
+                main_google_node: 'main'
+            }, 
+            events: {
+                get_info: 'get_tId_and_wId', 
+                inactive: 'inactive', 
+                active: 'active'
+            }, 
+            console: {
+                needs_to_be_updated: 'LeifiRemover selectors need to be updated!', 
+                removed: 'Leifi links were removed from this search.'
+            }, 
+            observerConfig: { childList: true, subtree: true }
+        }, 
+        init: function() {
+            var mainGoogleNode = document.getElementById(this.constants.queries.main_google_node);
+            /* avoiding google new tab page and other variations */
+            if(!mainGoogleNode) {
+                return chrome.runtime.sendMessage({ event: this.constants.events.inactive, url: window.location.href });
+            } 
+            chrome.runtime.sendMessage({ event: this.constants.events.get_info }, (info) => {
+                var tId = info.tId;
+                var wId = info.wId;
+                this.currentUrl[wId] = this.currentUrl[wId] ? this.currentUrl[wId] : {};
+                this.currentUrl[wId][tId] = window.location.href;
+                this.remove(info);
+                this.createResultsObserver(mainGoogleNode);
+            });
+        }, 
+        getAllW3Links: function() {
+            return document.querySelectorAll(this.constants.queries.result_links);
+        }, 
+        remove: function(info) {
+            var tId = info.tId;
+            var wId = info.wId;
+            var links = this.getAllW3Links();
+            var count = links.length;
+            if(!count) {
+                if(!this.isSameUrl(window.location.href, info)) {
+                    chrome.runtime.sendMessage({ event: this.constants.events.inactive });
+                    this.currentUrl[wId][tId] = window.location.href;
+                }
+                return;
+            }
+            this.currentUrl[wId][tId] = window.location.href;
+            chrome.runtime.sendMessage({ event: this.constants.events.active, count: count });
+            console.info(count + ' ' + this.constants.console.removed);
+            links.forEach(this.deleteOldGrandpaNode.bind(this));
+        }, 
+        createResultsObserver: function(mainGoogleNode) {
+            this.resultsObserver = new MutationObserver(() => {
+                chrome.runtime.sendMessage({ event: this.constants.events.get_info }, info => {
+                    var tId = info.tId;
+                    var wId = info.wId;
+                    this.currentUrl[wId] = this.currentUrl[wId] ? this.currentUrl[wId] : {};
+                    this.remove(info);
+                });
+            });
+            this.resultsObserver.observe(mainGoogleNode, this.constants.observerConfig);
+        }, 
+        isSameUrl: function(currentUrl, info) {
+            var tId = info.tId;
+            var wId = info.wId;
+            return this.currentUrl[wId][tId] === currentUrl;
+        }, 
+        deleteOldGrandpaNode: function(el) {
+            var parent = el.closest(this.constants.queries.link_parent_node);
+            if(!parent) return console.warn(this.constants.console.needs_to_be_updated);
+            parent.style.display = 'none';
+        }
+    };
+
+    /* may need to tune this timeout in the future
+    otherwise we get progressive removals instead of all them toghether */
+    setTimeout(() => {
+        LeifiRemover.init();
+    }, 250)
+
+})();
